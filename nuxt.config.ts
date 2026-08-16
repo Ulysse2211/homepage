11f6fa052361438e727405e562e577aa4d3f91b0
Ulysse2211 <ulysse.gresselugue@gmail.com>
2026-08-16 12:10:55 +0200

    added sitemap module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content', 'shadcn-nuxt', '@nuxtjs/color-mode', '@nuxt/ui'],
+  modules: [
+    '@nuxt/content',
+    'shadcn-nuxt',
+    '@nuxtjs/color-mode',
+    '@nuxt/ui',
+    '@nuxtjs/sitemap'
+  ],
   compatibilityDate: '2025-07-15',
   css: ['~/assets/css/main.css'],
   devtools: { enabled: true },
